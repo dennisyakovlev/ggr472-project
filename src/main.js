9752c8f48b0ca75b4b3c0ea4c55a37ed9594110d
Dennis Yakovlev <dennis.yakovlev@yahoo.ca>
2024-03-30 22:21:38 -0400

    loader

--- src/main.js
+++ src/main.js
@@ -8,76 +8,164 @@ const map = new mapboxgl.Map({
     pitch: 45
 });
 
-function addDataAndLayers()
+const DATA_NAME = {
+    MAIN: 'data-main',
+    SITES: 'data-sites',
+    PLANTS: 'data-plants'
+};
+
+var DATA = {};
+var fetched = 0;
+
+fetch('https://raw.githubusercontent.com/dennisyakovlev/ggr472-project/master/data/data.geojson')
+    .then(response => response.json())
+    .then(response => {
+        console.log('a')
+        DATA[DATA_NAME.MAIN] = response;
+        fetched += 1;
+    })
+    .catch(err => {
+        throw new Error('Problem loading');
+    });
+
+
+fetch('https://raw.githubusercontent.com/dennisyakovlev/ggr472-project/master/data/contaminated_sites.geojson')
+    .then(response => response.json())
+    .then(response => {
+        console.log('b')
+        DATA[DATA_NAME.SITES] = response;
+        fetched += 1;
+    })
+    .catch(err => {
+        throw new Error('Problem loading');
+    });
+
+fetch('https://raw.githubusercontent.com/dennisyakovlev/ggr472-project/master/data/water_facilities.geojson')
+    .then(response => response.json())
+    .then(response => {
+        console.log('c')
+        DATA[DATA_NAME.PLANTS] = response;
+        fetched += 1;
+    })
+    .catch(err => {
+        throw new Error('Problem loading');
+    });
+
+function addData()
 {
-    map.addSource('sites-data', {
+    // main data
+    map.addSource(DATA_NAME.MAIN, {
         'type': 'geojson',
-        'data': 'https://raw.githubusercontent.com/dennisyakovlev/ggr472-project/master/data/contaminated_sites_ON.geojson'
+        'data': DATA[DATA_NAME.MAIN]
     });
 
-    map.addSource('air-data', {
+    // contaminated sites
+    map.addSource(DATA_NAME.SITES, {
         'type': 'geojson',
-        'data': 'https://raw.githubusercontent.com/dennisyakovlev/ggr472-project/master/data/airpollution_subset.geojson'
+        'data': DATA[DATA_NAME.SITES]
     });
 
+    // treatment plants
+    map.addSource(DATA_NAME.PLANTS, {
+        'type': 'geojson',
+        'data': DATA[DATA_NAME.PLANTS]
+    });
+}
 
-    var airData = map.addLayer({
-        'id': 'air-data-layer',
-        'type': 'fill-extrusion',
-        'source': 'air-data',
+function addLayers()
+{
+    // main data
+    var dataFill = map.addLayer({
+        'id': 'data-layer',
+        'type': 'fill',
+        'source': DATA_NAME.MAIN,
+        'layout': {},
         'paint': {
-            
+            'fill-color': 'red'
         }
     });
-    map.addLayer({
-        'id': 'air-data-layer-border',
+    var dataBorders = map.addLayer({
+        'id': 'data-border',
         'type': 'line',
-        'source': 'air-data',
+        'source': DATA_NAME.MAIN,
+        'layout': {},
         'paint': {
-            'line-color': 'black'
+            'line-color': 'blue'
         }
     });
 
-    var sitesData = map.addLayer({
-        'id': 'sites-data-layer',
+    // contaimated sites
+    var sitesCircle = map.addLayer({
+        'id': 'sites-layer',
         'type': 'circle',
-        'source': 'sites-data',
+        'source': DATA_NAME.SITES,
         'paint': {
-            'circle-color': 'red'
+            'circle-color': 'purple'
         }
     });
+
+    // treatment plants
+    var plantsCircle = map.addLayer({
+        'id': 'plants-layer',
+        'type': 'circle',
+        'source': DATA_NAME.PLANTS,
+        'paint': {
+            'circle-color': 'orange'
+        }
+    });
+}
+
+function dataParse(data)
+{
+    const features = turf.featureCollection(data.features);
+    const bbox = turf.envelope(features);
+    const poly = turf.polygon(bbox.geometry.coordinates);
+    const bboxTrans = turf.transformScale(poly.geometry, 1.2);
+    const newBbox = turf.envelope(bboxTrans);
+    const hexGrid = turf.hexGrid(newBbox.bbox, 50, 'kilometers');
+    const collected = turf.collect(hexGrid, data, "FederalSiteIdentifier", "values")
+
+    return collected
+}
+
+function addHexGrid()
+{
+    // const processedData = dataParse();
+}
+
+function loadingRemove()
+{
+    $('.loader').css('animation', 'l15 1s infinite linear, loader-key-anim-out 1s forwards ease-in');
+    $('#screen-2').addClass('screen-anim-in');
 }
 
 map.on("load", () => {
     map.addControl(new mapboxgl.NavigationControl()); // add nav controls
 
-    addDataAndLayers();
+    var count = 0;
+    var dontQuit = false;
+    const timer = setInterval(function() {
+        console.log(fetched)
+        if (fetched == 3 && dontQuit == false)
+        {
+            dontQuit = true;
 
+            addData();
+            addLayers();
+            addHexGrid();
 
-    // const btn1 = new Button('btn-1');
-    // var stateTransitions = 1;
-    // btn1.addOnFunc(function(e) {
-    //     transitionFunc(e, stateTransitions);
-    //     stateTransitions += 1;
-    // });
-    // btn1.addOffFunc(function(e) {
-    //     transitionFunc(e, stateTransitions);
-    //     stateTransitions += 1;
-    // });
+            console.log('READY')
+            setTimeout(e => loadingRemove(), 1000);
 
-    // const menu = new Menu('menu-1');
-    // menu.enableAnim();
-
-    // var stateTransitions = 1;
-    // menu.addOnFunc(function(e, state) {
-    //     console.log('IN')
-    //     // transitionFunc(e, stateTransitions);
-    //     // stateTransitions += 1;
-    // });
-    // menu.addOffFunc(function(e, state) {
-    //     console.log('OUT')
-    // }); 
-
+            clearInterval(timer);
+        }
+        if (count == 28 && dontQuit == false)
+        {
+            loadFail();
+            clearInterval(timer);
+        }
+        count += 1;
+    }, 250);
 });
 
 $(document).ready(function() {
